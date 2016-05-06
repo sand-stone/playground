8d5b5d359f09546de212c5322b6e663676c94f46
Sand Stone <sand.m.stone@gmail.com>
2016-05-05 19:26:55 -0700

    build script

---
+++ samples/b.sh
@@ -0,0 +1,2 @@
+javac -cp ./kudu-client-0.9.0-SNAPSHOT.jar:./async-1.4.1.jar:.:./slf4j-log4j12-1.7.12.jar:./slf4j-api-1.7.12.jar:./log4j-1.2.17.jar:joda-time-2.9.3.jar TimeseriesReader.java 
+javac -cp ./kudu-client-0.9.0-SNAPSHOT.jar:./async-1.4.1.jar:.:./slf4j-log4j12-1.7.12.jar:./slf4j-api-1.7.12.jar:./log4j-1.2.17.jar:joda-time-2.9.3.jar TimeseriesGenerator.java 
