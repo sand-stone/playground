1ec71ce100bc25b3d7247d9e5f671bd31c4e351b
Sand Stone <sand.m.stone@gmail.com>
2016-05-08 20:53:02 -0700

    setup kudu

--- samples/setup.py
+++ samples/setup.py
@@ -1,5 +1,5 @@
 import os,subprocess
-num=4
+num=72
 dirs=[]
 for i in range(0,num):
     dirs.append('/mnt/db/data'+`i`)
@@ -7,7 +7,7 @@ for d in dirs:
     os.system("rm -rf "+d)
     os.system("mkdir "+d)
 cmds=[]
-c=['./bin/kudu-master','--use_hybrid_clock=false','--cfile_do_on_finish=flush','--flush_threshold_mb=1000','-default_num_replicas=1','--logtostderr','-fs_wal_dir=']
+c=['./bin/kudu-master','--use_hybrid_clock=false','--cfile_do_on_finish=flush','--flush_threshold_mb=5000','-rpc_num_service_threads=24','-num_reactor_threads=12', '-default_num_replicas=1','--logtostderr','-fs_wal_dir=']
 c[-1]=c[-1]+dirs[0]
 cmds.append(c)
 p1=7052
@@ -22,7 +22,9 @@ for i in range(1,num):
     t.append(ws)
     t.append('--use_hybrid_clock=false')
     t.append('--cfile_do_on_finish=flush')
-    t.append('--flush_threshold_mb=1000')
+    t.append('--flush_threshold_mb=100000')
+    t.append('-rpc_num_service_threads=24')
+    t.append('-num_reactor_threads=12')
     t.append('--logtostderr')
     t.append('-fs_wal_dir='+dirs[i])
     cmds.append(t)
